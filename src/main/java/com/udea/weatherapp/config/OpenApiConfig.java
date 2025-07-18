4ac62409a215b58594f759288c0b566c1be28313
rodardila <rodrigo.ardila@udea.edu.co>
2025-07-18 17:27:35 -0500

    Adds OpenAPI configuration
    Adds OpenAPI configuration for API documentation.
    
    This configuration defines the API title, version, description, terms of service, and license information.

---
+++ src/main/java/com/udea/weatherapp/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.udea.weatherapp.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Weather API")
+                        .version("1.0")
+                        .description("API RESTful para consultar y registrar datos del clima de ciudades. Proyecto final de Arquitectura de Software.")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
